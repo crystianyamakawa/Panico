b49026b04a16925fd13721f34c5e81e10deea64e
Crystian Yamakawa <crystian_yamakawa@hotmail.com>
2020-06-20 17:38:51 -0300

    2006

--- app/src/main/java/com/crystian/panico/MainActivity.java
+++ app/src/main/java/com/crystian/panico/MainActivity.java
@@ -20,11 +20,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.crystian.panico.R;
+import com.crystian.panico.model.PanicoConfig;
 
 public class MainActivity extends AppCompatActivity {
     Button btnGps;
     TextView txtLatitude, txtLongitude;
-
+    Location minhaLocalizacao;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,10 +56,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void TesteLigar(View v) {
+        EditText txTelefone = (EditText) findViewById(R.id.txTelefone);
+        String numero = txTelefone.getText().toString();
+        Uri uri = Uri.parse("tel:" + numero);
 
-    public void Ligar(View v) {
-        EditText telefone = (EditText) findViewById(R.id.txTelefone);
-        String numero = telefone.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE},1);
+            return;
+        }
+        startActivity(intent);
+
+    }
+
+    public void Ligar(String telefone) {
+        String numero = telefone;
         Uri uri = Uri.parse("tel:" + numero);
 
         Intent intent = new Intent(Intent.ACTION_CALL, uri);
@@ -103,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
             LocationListener locationListener = new LocationListener() {
                 public void onLocationChanged(Location location) {
                     atualizar(location);
+                    minhaLocalizacao = location;
                 }
 
                 public void onStatusChanged(String provider, int status, Bundle extras) { }
@@ -119,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void atualizar(Location location)
     {
+
         Double latPoint = location.getLatitude();
         Double lngPoint = location.getLongitude();
 
@@ -129,6 +144,41 @@ public class MainActivity extends AppCompatActivity {
     public void chamarConfig(View view){
         Intent intent = new Intent(this, ConfigActivity.class);
         startActivity(intent);
+
+    }
+
+    public void AcionaBotaoPanico(View v){
+        PanicoConfig config = PanicoConfig.findById(PanicoConfig.class,(long) 1);
+        Toast.makeText(this, "tel1:"+config.getTelefone1() + "tel2:"+config.getTelefone2() + "tel3:"+config.getTelefone3(), Toast.LENGTH_LONG).show();
+
+        // Habilitar GPS e Pegar Posição
+    //    pedirPermissoes();
+
+       // Double latPoint = minhaLocalizacao.getLatitude();
+       // Double lngPoint = minhaLocalizacao.getLongitude();
+
+
+        // Enviar SMS
+        String telefone1 = config.getTelefone1();
+        String telefone2 = config.getTelefone2();
+        String telefone3 = config.getTelefone3();
+
+        String MensagemSMS = "ME AJUDEM!!! - Lat: "  + "latPoint.toString()" + "Log: " +"lngPoint.toString()";
+        SmsManager smgr = SmsManager.getDefault();
+        smgr.sendTextMessage(telefone1,null,MensagemSMS,null,null);
+        Toast.makeText(this, "SMS tel1:"+config.getTelefone1(), Toast.LENGTH_LONG).show();
+        smgr.sendTextMessage(telefone2,null,MensagemSMS,null,null);
+        Toast.makeText(this, "SMS tel2:"+config.getTelefone2() , Toast.LENGTH_LONG).show();
+        smgr.sendTextMessage(telefone3,null,MensagemSMS,null,null);
+        Toast.makeText(this, "SMS tel3:"+config.getTelefone3(), Toast.LENGTH_LONG).show();
+
+        // Ligar
+        Ligar(telefone1);
+        Toast.makeText(this, "Ligou tel1:"+config.getTelefone1(), Toast.LENGTH_LONG).show();
+        Ligar(telefone2);
+        Toast.makeText(this, "Ligou tel2:"+config.getTelefone2(), Toast.LENGTH_LONG).show();
+        Ligar(telefone3);
+        Toast.makeText(this, "Ligou tel3:"+config.getTelefone3(), Toast.LENGTH_LONG).show();
 
     }
 
